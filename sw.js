34f28170fdc1d5ca83533c2d21da97af8f708e82
cmichael871-code <c.michael871@gmail.com>
2025-12-10 13:10:39 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,26 @@
-const CACHE_NAME = 'nhs-midwife-v8'; // Bumped to v8
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'nhs-midwife-v9'; // Version 9
+
+const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './icon.png'
 ];
 
-self.addEventListener('install', (event) => {
-  self.skipWaiting(); 
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)));
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((keyList) => {
-    return Promise.all(keyList.map((key) => {
-      if (key !== CACHE_NAME) return caches.delete(key);
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => {
+    return Promise.all(keys.map((k) => {
+      if (k !== CACHE_NAME) return caches.delete(k);
     }));
   }));
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((cachedResponse) => {
-    return cachedResponse || fetch(event.request);
-  }));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
