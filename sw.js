7693290b6a87fe0f3afbcf97930148d9660d0b1a
cmichael871-code <c.michael871@gmail.com>
2025-12-10 08:44:29 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nhs-midwife-v6'; // Version bump to force update
+const CACHE_NAME = 'nhs-midwife-v7'; // Updated to v7
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -6,23 +6,18 @@ const ASSETS_TO_CACHE = [
   './icon.png'
 ];
 
-// 1. INSTALL: Force new worker to take over immediately
 self.addEventListener('install', (event) => {
   self.skipWaiting(); 
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
-// 2. ACTIVATE: Delete ALL old caches to clean up "Zombie" files
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
         if (key !== CACHE_NAME) {
-          console.log('Removing old cache:', key);
           return caches.delete(key);
         }
       }));
@@ -31,7 +26,6 @@ self.addEventListener('activate', (event) => {
   return self.clients.claim();
 });
 
-// 3. FETCH: Serve from cache, but fall back to network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
