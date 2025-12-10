97eb1ace08ca28cb3b01efddd08f6b5b4a3c97db
cmichael871-code <c.michael871@gmail.com>
2025-12-10 07:34:59 +0000

    Initial upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'nhs-midwife-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// 1. Install Service Worker & Cache Files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching offline assets');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 2. Serve Cached Content when Offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Return cached file if found, otherwise try network
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+// 3. Update Cache (Delete old versions if name changes)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
